e9e697fc436dedd300bd5acb5b6401f59607ac42
geoffbonneau <gbonneau@silvercloudinc.com>
2016-05-11 16:07:00 -0400

    First two scripts
    These will turn all connected lights on or off

---
+++ lightify-all-off.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# You need to install ja to parse the json returned with the auth token
+# API reference: https://us.lightify-api.org/lightify/
+Authtoken=$(curl -s -H 'Content-Type: application/json' -X POST -d '{"username" : "LIGHTIFY ACCOUNT EMAIL", "password" : "PLACEHOLDER", "serialNumber" : "OSR0XXXXXX"}' https://us.lightify-api.org/lightify/services/session | jq ."securityToken" | sed 's/\"//g')
+
+curl -s -H 'Content-Type: application/json' -H "authorization:$Authtoken" -X GET  https://us.lightify-api.org/lightify/services/device/all/set?onoff=0
+
+exit 0
